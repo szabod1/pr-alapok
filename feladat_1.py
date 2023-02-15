529de68630c5f35acfa09cd5c0433b2d7a486002
szabod1 <112930892+szabod1@users.noreply.github.com>
2023-02-15 07:45:30 +0100

    Add files via upload

---
+++ feladat_1.py
@@ -0,0 +1,8 @@
+#Szabó Dorián 2023.02.15
+szam = int(input('Adj meg egy egész számot: '))
+if szam > 0:
+    print('A megadott szám pozitív.')
+elif szam < 0:
+    print('A megadott szám negatív.')  
+else:
+    print('A megadott szám nulla')      
