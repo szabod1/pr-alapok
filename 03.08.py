8955e81f2b137e2edb14f6dcd76923ae0320e156
szabod1 <112930892+szabod1@users.noreply.github.com>
2023-03-08 07:51:14 +0100

    Add files via upload

---
+++ 03.08.py
@@ -0,0 +1,12 @@
+#Szabo Dorián 10/c A
+
+
+
+szam_sorozat = [15, 4, 5, 19, 2]
+
+darab = 0
+for szam in szam_sorozat:
+    if szam * 0 == 0:
+       darab = darab + 1
+
+print('Ennyi darab:', darab)
