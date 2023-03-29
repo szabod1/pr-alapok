9d4c94cc3cf0e4e5fc1ea80afe92abc910827c25
szabod1 <112930892+szabod1@users.noreply.github.com>
2023-03-29 08:23:40 +0200

    Add files via upload

---
+++ fuggveny_01.py
@@ -0,0 +1,8 @@
+#ord()		   A Python ord() függvény egyetlen Unicode karakterből álló string argumentumot vesz fel, és egész számot ad vissza a Unicode kódpont értékéből.
+#chr()         A chr() függvény a megadott unicode-ot képviselő karaktert adja vissza.
+
+szám_sor = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+print(min(szám_sor))
+print(max(szám_sor))
+print(szám_sor[2])
---
+++ fuggveny_02.py
@@ -0,0 +1,21 @@
+#lower()		    Ez a függvény új karakterláncot hoz létre kisbetűkkel.
+#upper()		    Egy karakterláncot nagybetűssé alakíthatunk Pythonban az str.upper() függvény segítségével.
+#capitalize()	    Python String kapitalize() függvény a karakterlánc nagybetűs változatát adja vissza.
+
+#endswith()	        A Python string endswith() függvény igaz értéket ad vissza, ha a karakterlánc a megadott utótaggal végződik, ellenkező esetben False értéket ad vissza.
+#find()		        A Python String find() metódus a karakterláncban lévő részkarakterlánc indexének megkeresésére szolgál.
+#isalnum()	        A Python string isalnum() függvény True értéket ad vissza, ha csak alfanumerikus karakterekből áll.
+#isalpha()	        Python String isalpha() függvény True értéket ad vissza, ha a karakterlánc összes karaktere ábécé, ellenkező esetben False.
+#islower()  	    Python String islower() igazat ad vissza, ha a karakterláncban minden kis- és nagybetűs karakter kisbetűs, és van legalább egy kis- és nagybetűs karakter, ellenkező esetben False értéket ad vissza.
+#join()		        Ez a függvény egy új karakterláncot ad vissza, amely a karakterláncok összefűzése iterálható karakterlánc objektummal elválasztóként.
+#replace()	        Python string change() függvény egy új karakterlánc létrehozására szolgál egy másik karakterlánc egyes részeinek cseréjével.
+#rfind()            A metódus megkeresi a megadott érték utolsó előfordulását.
+#rstrip()           A metódus eltávolítja a záró karaktereket (a karakterlánc végén lévő karaktereket), a szóköz az alapértelmezett eltávolítandó karakter.
+#startswith()	    A Python string startswith() függvény igaz értéket ad vissza, ha a karakterlánc a megadott előtaggal kezdődik, ellenkező esetben False értéket ad vissza.
+#strip()		    A karakterlánc objektumból a szóközök levágására szolgál.
+#swapcase()	        A Python String csere
+#title()		    A Python String title() függvény a karakterlánc kis- és nagybetűs változatát adja vissza.
+#center()	        A Python string center() függvény egy megadott méretű középre igazított karakterláncot ad vissza.
+
+
+
